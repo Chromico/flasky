4a3ff76cd03bdb97a23f290b0a5d67df504244a8
Chromico <32539454+Chromico@users.noreply.github.com>
2018-12-06 00:44:09 -1000

    Add files via upload

---
+++ mongo.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+
+mongo = PyMongo(app)
+
+@app.route('/star', methods=['POST'])
+def posted_JSON():
+
+    data = request.get_json()
+    print(data)
+    return "JSON RECEIVED"
